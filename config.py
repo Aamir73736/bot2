f492a3302810e047c7f35f2d792fa4baa267c2ae
Aamir73736 <Ytt14569@gmail.com>
2024-10-26 23:31:35 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7947052252:AAESD5GY_oB9FWMahq3u2bX-4nOb9a7M3l0")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Bluryf4ce")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6965153309")
+
+
+
+
+
+
+
+  
